8b32df5dde047e5fa1136ed33297e9781d3c08c9
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-09-12 09:09:32 +0200

    Change nav layout

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,18 +1,46 @@
 import './header.css';
 
+const links = Array.from(document.querySelectorAll('nav a'));
+const selector = document.querySelector('nav .selector');
+const nav = document.querySelector('nav');
+
+const getActiveLink = function getActiveLink() {
+  return document.querySelector('nav a.active');
+};
+
+const getActiveWidth = function getActiveWidth() {
+  return getActiveLink().getBoundingClientRect().width;
+};
+
+const getSelectorLeftPos = function getSelectorLeftPosition() {
+  return getActiveLink().getBoundingClientRect().left - nav.getBoundingClientRect().left;
+};
+
+const removeActiveClass = function removeActiveClass() {
+  links.forEach((link) => {
+    link.classList.remove('active');
+  });
+};
+
+const updateSelector = function updateSelector() {
+  selector.style.left = `${getSelectorLeftPos()}px`;
+  selector.style.width = `${getActiveWidth()}px`;
+};
+
+const addNavListeners = function addNavListeners() {
+  links.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      removeActiveClass();
+      link.classList.add('active');
+      updateSelector();
+    });
+  });
+};
+
 const initiateHeader = function initiateHeader() {
-  const open = document.querySelector('.open');
-  const close = document.querySelector('.close');
-  const container = document.querySelector('.container');
-
-  open.addEventListener('click', () => {
-    container.classList.add('show-nav');
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  });
-
-  close.addEventListener('click', () => {
-    container.classList.remove('show-nav');
-  });
+  addNavListeners();
+  updateSelector();
 };
 
 export default initiateHeader;
