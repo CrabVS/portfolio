99bd21b5b160a2e6f769929fb32381efa0937241
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-09-12 11:07:42 +0200

    Add structure and animation to skills

---
+++ src/components/skills/skills.js
@@ -0,0 +1,114 @@
+import './skills.css';
+
+const skillIcons = Array.from(document.querySelectorAll('.head-icon .skill-icon'));
+const skillsContainer = document.querySelector('.skills-container');
+
+const updateSkillIconsContainer = function updateSkillIconsContainer() {
+  const skillIcon = document.querySelector('.head-icon .skill-icon');
+  if (skillIcon === null) {
+    const headContainer = document.querySelector('.head-container');
+    const headIcon = document.querySelector('.head-icon');
+    headIcon.style.height = '0px';
+    headContainer.style.height = '0px';
+    setTimeout(() => {
+      headContainer.remove();
+    }, 1000);
+  }
+};
+
+const getSkillCards = function getSkillCards() {
+  const skillCards = Array.from(document.querySelectorAll('.skills-container .skill-card'));
+
+  return skillCards;
+};
+
+const createNewCard = function createNewCard() {
+  const card = document.createElement('div');
+  card.classList.add('skill-card');
+
+  skillsContainer.appendChild(card);
+};
+
+const moveIcon = function moveIconToCard(index, skillCards) {
+  const headIcon = document.querySelector('.head-icon');
+  const headLeft = headIcon.getClientRects()[0].left;
+  const headTop = headIcon.getClientRects()[0].top;
+
+  const lastCard = skillCards[skillCards.length - 1];
+  const cardTopPos = lastCard.getClientRects()[0].top;
+  const cardLeftPos = lastCard.getClientRects()[0].left;
+  const cardWidth = lastCard.getClientRects()[0].width;
+
+  const nextTopPos = cardTopPos - headTop;
+  const nextLeftPos = cardLeftPos - headLeft;
+  skillIcons[index].style.top = `${nextTopPos}px`;
+  skillIcons[index].style.left = `${nextLeftPos + cardWidth / 2}px`;
+  skillIcons[index].style.opacity = 0;
+
+  lastCard.style.opacity = 1;
+
+  setTimeout(() => {
+    skillIcons[index].remove();
+    updateSkillIconsContainer();
+  }, 800);
+};
+
+const revealIcon = function revealIconFromHead(index) {
+  skillIcons[index].style.opacity = 1;
+  skillIcons[index].style['z-index'] = 1;
+
+  let switchValue = index;
+
+  if (index > 7) {
+    switchValue = index % 8;
+  }
+
+  switch (switchValue) {
+    case 0:
+      skillIcons[index].style.top = '-50%';
+      break;
+    case 1:
+      skillIcons[index].style.top = '-50%';
+      skillIcons[index].style.left = '150%';
+      break;
+    case 2:
+      skillIcons[index].style.left = '150%';
+      break;
+    case 3:
+      skillIcons[index].style.left = '150%';
+      skillIcons[index].style.top = '150%';
+      break;
+    case 4:
+      skillIcons[index].style.top = '150%';
+      break;
+    case 5:
+      skillIcons[index].style.left = '-50%';
+      skillIcons[index].style.top = '150%';
+      break;
+    case 6:
+      skillIcons[index].style.left = '-50%';
+      break;
+    case 7:
+      skillIcons[index].style.left = '-50%';
+      skillIcons[index].style.top = '-50%';
+      break;
+    default:
+      break;
+  }
+
+  setTimeout(() => {
+    createNewCard();
+    const skillCards = getSkillCards();
+    moveIcon(index, skillCards);
+  }, 1000);
+};
+
+const initiateSkills = function initiateSkills() {
+  skillIcons.forEach((icon, index) => {
+    setTimeout(() => {
+      revealIcon(index);
+    }, 300 * (index + 1));
+  });
+};
+
+export default initiateSkills;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import './reset.css';
 import './style.css';
 
 import initiateHeader from './components/header/header';
+import initiateSkills from './components/skills/skills';
 
 initiateHeader();
+initiateSkills();
