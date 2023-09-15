0bbe49eec61a51e571f1916cfc37229586c7788e
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-09-15 13:03:10 +0200

    Add create page to vanilla

--- src/components/projects/vanilla-projects/vanilla.js
+++ src/components/projects/vanilla-projects/vanilla.js
@@ -1,9 +1,17 @@
 import './vanila.css';
 import createProject from '../projects';
 
-const projectsContainer = document.querySelector('.projects-container');
+const main = document.querySelector('main');
+
+const createPage = function createPage() {
+  main.innerHTML = `
+  <section class="projects-container">
+  </section>`;
+};
 
 const initiateVanilla = function initiateVanilla() {
+  createPage();
+  const projectsContainer = document.querySelector('.projects-container');
   projectsContainer.appendChild(createProject());
 };
 
